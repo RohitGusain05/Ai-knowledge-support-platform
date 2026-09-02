ac984478c8b42559d6ffef3f3f727660c35c15fe
RohitGusain05 <142705369+RohitGusain05@users.noreply.github.com>
2026-09-02 17:47:11 +0530

    feat: add Spring Boot application

---
+++ backend/src/main/java/com/rohitgusain/knowledge/KnowledgeSupportApplication.java
@@ -0,0 +1,12 @@
+package com.rohitgusain.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeSupportApplication.class, args);
+    }
+}
